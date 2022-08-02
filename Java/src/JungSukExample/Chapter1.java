781d5ba4394984ba73b6a35649b5a835a7fb689f
AhnYeonghoo <dksdudgn00@naver.com>
2022-08-02 15:50:58 +0900

    자바의정석 연습문제 1장

---
+++ Java/src/JungSukExample/Chapter1.java
@@ -0,0 +1,23 @@
+package JungSukExample;
+
+public class Chapter1 {
+    public static void main(String[] args) {
+        System.out.println("1" + "2"); // "12"
+        System.out.println(true + ""); // "true"
+        System.out.println('A' + 'B'); // 131
+        System.out.println('1' + 2);
+        System.out.println('1' + '2');
+        System.out.println('J' + "ava");
+        // b, c, d, e,
+        byte b = 10;
+        char ch = 'A';
+        int i = 100;
+        long l =  1000L;
+
+        b = (byte)i;
+        ch = (char) b;
+        short s = (short)ch;
+        float f = l;
+        i = ch;
+    }
+}
