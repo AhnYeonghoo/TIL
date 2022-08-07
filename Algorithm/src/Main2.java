7d315f7e35b2fdbb7ea8c8ba5cb7fa2bb87dd999
AhnYeonghoo <dksdudgn00@naver.com>
2022-08-08 07:33:53 +0900

    단ìReverse The Voca

---
+++ Algorithm/src/Main2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Main2 {
+    public static void main(String[] args) {
+        Main2 T = new Main2();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+        for (String x : T.solutionTwo(n, str)) {
+            System.out.println(x);
+        }
+    }
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public ArrayList<String> solutionTwo(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0;
+            int rt = x.length()-1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+
+    }
+}
