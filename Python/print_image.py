7bbe32149cc3c679060c894ea1e3528f4bcc63eb
AhnYeonghoo <dksdudgn00@naver.com>
2022-08-14 18:35:49 +0900

    Cat Movie Print

--- Python/print_image.py
+++ Python/print_image.py
@@ -6,3 +6,20 @@ image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
 cv2.imshow("image",image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+video = cv2.VideoCapture("cat2.mp4")
+
+while video.isOpened():
+    check, frame = video.read()
+    if not check:
+        print("Frame is done")
+        break
+    
+    cv2.imshow("cute cats", frame)
+    if cv2.waitKey(25) == ord('q'):
+        print("exit video")
+        break
+
+video.release()
+cv2.destroyAllWindows()
+    
--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-cv2.IMREAD_UNCHANGED
+path = "C:/Users/Ahn_YeongHoo/Desktop/TIL/Python/dog.jpg"
+# image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+
+# cv2.imshow("image",image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
