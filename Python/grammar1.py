3293aff71bce96910609e65f5bc8177482df087c
AhnYeonghoo <dksdudgn00@naver.com>
2022-08-14 16:48:34 +0900

    헷?갈리는 Python

---
+++ Python/grammar1.py
@@ -0,0 +1,72 @@
+print("Hello World")
+print(1)
+print(3.14)
+print("Hello World")
+print('hello World')
+print('10')
+print(True)
+print(False)
+
+variable = 10
+envelope1 = 10000
+envelope2 = 20000
+print(variable)
+print(envelope1)
+print(envelope2)
+
+print(2+1)
+print(2+(int)("10"))
+int('2')
+float('1.5')
+str(2)
+num = int(float(3.14))
+print(num)
+print(5 % 2)
+print(5 // 2)
+print(5 ** 2)
+print(5 + 2)
+print(5 - 2)
+print(5 * 2)
+print(5 > 2)
+print(5 >= 2)
+print(5 < 2)
+print(5 <= 2)
+print(5==2)
+print(5!=2)
+print(3<5 and 7 < 5)
+print(3 < 5 or 7 < 5)
+print(not 3 < 5)
+print('c' in 'cat')
+print('c' not in 'cat')
+a = 1
+b = 2
+c = 3
+d = 4
+print(a >= b and c <= d)   
+a = {'name': '2'}
+print(a['name'])
+a = [1, 2,3 , 4, 5]
+for i in a:
+    print(i)
+class BlackBox:
+    pass
+b1 = BlackBox()
+print(b1)
+b1.name = 'dd'
+print(b1.name)
+print(isinstance(b1, BlackBox))
+class BlackBox2:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price    
+b2 = BlackBox2("dd2", 20000)
+print(b2.name, b2.price)
+b3 = BlackBox2("dd3", 40000)
+print(b3.name, b3.price)
+#  아 맞다 파이썬 주석 #이었지.. // /**/랑 헷갈리누..
+
+
+        
+        
+    
+
