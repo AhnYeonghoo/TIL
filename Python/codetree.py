c7a97bef65c5231ca2786927986cff96748dc9d0
AhnYeonghoo <dksdudgn00@naver.com>
2022-08-25 14:15:48 +0900

    CodeTree Tutorial

--- Python/codetree.py
+++ Python/codetree.py
@@ -1,8 +1,79 @@
-n =  int(input())
-m = int(input())
+# n =  int(input())
+# m = int(input())
 
 
-for i in range(1, n+1):
-    if ((n % i == 0) and (m % i == 0)):
-        print(i)
-        break
+# for i in range(1, n+1):
+#     if ((n % i == 0) and (m % i == 0)):
+#         print(i)
+#         break
+
+# moon = 0.165000
+# weight = 13
+# print(f"{weight} * {format(moon,'.6f')} = {format(weight * moon,'.6f')}")
+
+a = 25.352
+print(format(a, '.1f'))
+
+
+print('9.2ft = 280.4cm')
+print('1.3mi = 209214.2cm')
+
+a = 5.26
+b = 8.27
+print(format(a*b, '.3f'))
+
+a = 6
+print(a)
+
+a = 7
+a = 4
+print(a)
+
+a= 'C'
+a = 'T'
+print(a)
+
+a = 5
+b = 3
+a = b
+print(a)
+print(b)
+a = 2
+b = 6
+a = b
+print(a)
+print(b)
+a = 3
+b = 4
+b = a
+print(a, b)
+print(a*b)
+a = 3
+b = 5
+a, b = b, a
+print(a)
+print(b)
+a = 2
+b = 5
+a, b = b, a
+print(a)
+print(b)
+a = 5
+b = 6
+c = 7
+temp = b
+b = a
+a, c = c, temp
+print(a)
+print(b)
+print(c)
+a = 1
+b = 2
+c = 3
+a=b = c
+print(a, b, c)
+a = 5
+b = 6
+c = 7
+a =b = c
+print(a, b, c)
