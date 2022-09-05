443965a44678d4616bf71c6217b346e84193817d
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-06 07:28:52 +0900

    Python Practice

--- Python/day8/ng.py
+++ Python/day8/ng.py
@@ -86,3 +86,47 @@ print(where_is(Point(1,y=var)))
 print(where_is(Point(x=1,y=3)))
 print(where_is(Point(y=1,x=3)))
 
+import matplotlib.pyplot as plt
+sales = [1,5,7,3,7]
+month = [3, 4, 5, 6, 7]
+plt.rc('font')
+plt.title('월별 판매 실적')
+plt.bar(month,sales, color='b')
+plt.show()
+sales = [1, 5, 7, 3, 7]
+plt.title('월별 판매 실적')
+plt.barh(range(3, 8), sales, color='b')
+plt.show()
+sales = [1, 5, 7, 3, 7]
+plt.hist(sales)
+plt.show()
+import random
+a = []
+for i in range(10):
+  a.append(random.randint(1, 10))
+print(a)
+plt.hist(a)
+plt.show()
+a = [7, 5, 3, 3, 1, 3, 2, 10 ,7, 8]
+plt.hist(a, bins=20)
+plt.show()
+b_type=[25, 19, 37, 11]
+plt.pie(b_type)
+plt.show()
+b_type=[25, 19, 37, 11]
+plt.pie(b_type, labels=['A', 'B', 'O', 'AB'])
+plt.show()
+b_type = [25, 19, 37, 11]
+labels = ['A', 'B', 'O', 'AB']
+plt.pie(b_type, labels=labels, autopct='%1.2f%%')
+plt.show()
+kor = [80, 20, 50, 20, 10, 50, 60 ,30 ,60]
+eng = [90, 40, 60, 40, 10, 30, 50, 70, 90]
+plt.scatter(kor, eng)
+plt.show()
+kor = [80, 20, 50, 20, 10, 50, 60 ,30 ,60]
+eng = [90, 40, 60, 40, 10, 30, 50, 70, 90]
+plt.scatter(kor, eng, color='red', alpha=0.3)
+plt.xlabel('국어')
+plt.ylabel('영어')
+plt.show()
