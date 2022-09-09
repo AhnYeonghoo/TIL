838e24143cb45abb66abffc4825690d2df42eee3
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-10 07:26:23 +0900

    Python Threed

---
+++ Python/day10/threed.py
@@ -0,0 +1,28 @@
+import threading, zipfile
+import logging
+
+class AsyncZip(threading.Thread):
+    def __init__(self, infile, outfile):
+        threading.Thread.__init__(self)
+        self.infile = infile
+        self.outfile = outfile
+
+    def run(self):
+        f = zipfile.ZipFile(self.outfile, 'w', zipfile.ZIP_DEFLATED)
+        f.write(self.infile)
+        f.close()
+        print('Finished background zip of:', self.infile)
+
+background = AsyncZip('mydata.txt', 'myarchive.zip')
+background.start()
+print('The main program continues to run in foreground.')
+
+background.join()
+print('Main program waited until background was done.')
+
+
+logging.debug('Debugging information')
+logging.info('Informational message')
+logging.warning('Warning:config file %s not found', 'server.conf')
+logging.error('Error occurred')
+logging.critical('Critical error -- shutting down')
