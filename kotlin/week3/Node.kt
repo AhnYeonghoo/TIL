07bc78bfd087c738a7fd20431564d2b8db4af6d1
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-18 08:06:56 +0900

    DataStructure week3 selfStudy

---
+++ kotlin/week3/Node.kt
@@ -0,0 +1,20 @@
+package week3
+
+data class Node<T>(var value: T, var next: Node<T>? = null) {
+    override fun toString(): String {
+        return if (next != null) {
+            "$value -> ${next.toString()}"
+        } else {
+            "$value"
+        }
+    }
+}
+
+fun main() {
+    val node1 = Node(value = 1)
+    val node2 = Node(value = 2)
+    val node3 = Node(value = 3)
+    node1.next = node2
+    node2.next = node3
+    println(node1)
+}
