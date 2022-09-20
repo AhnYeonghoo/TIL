7ea86a0364fbd1b73df615882f62c27c9a65e38e
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-21 07:48:24 +0900

    셀프넘버 with Kotlin

---
+++ Algorithm/src/4673.kts
@@ -0,0 +1,20 @@
+import java.util.*
+fun main() = with(Scanner(System.`in`)) {
+    var array = IntArray(10001) { i -> i + 1}
+
+    for (i in 0 until array.size) {
+        val temp = sum(i.toString())
+        if (temp > 10000) continue
+        array[temp] = 0
+    }
+    for (i in 0 until array.size) {
+        if (array[i] != 0) println("${array[i] - 1}")
+    }
+}
+
+fun sum(sequence: String): Int {
+    var temp: Int = sequence.toInt()
+    for (i in 0 until sequence.length)
+        temp += sequence.substring(i, i + 1).toInt()
+    return temp
+}
