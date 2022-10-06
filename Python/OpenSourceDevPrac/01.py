0b85eb2d1d7ca451d29e5198dcf666676213c490
AhnYeonghoo <dksdudgn00@naver.com>
2022-10-06 18:03:55 +0900

    Python

---
+++ Python/OpenSourceDevPrac/01.py
@@ -0,0 +1,38 @@
+# 문자열 출력하기
+print('Hello World!')
+print('Python Programming')
+print('Hello, world!\nHello, world!')
+
+# Unit 04
+print('Hello'); print('1234')
+
+# Hello, world! 출력
+print('HEllo, world!')
+# comments
+
+a = 1 + 2
+print('Hello World!')
+if a == 10:
+    print('10 입니다.')
+print(1+1)
+print(1-2)
+print(1*2)
+print(5/2)
+print(5//2)
+print(4//2)
+print(5 % 2)
+print(2 ** 10)
+int(3.3)
+print(int(5 / 2))
+print(int('10'))
+print(type(10))
+print(divmod(5, 2))
+quotient, remainer = divmod(5, 2)
+print(quotient, remainer)
+a = 0b110
+b = 0o10
+c = 0xF
+print(a, b, c)
+print(int(0.2467 * 12 + 4.159))
+
+
