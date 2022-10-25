9dc429d44feca84a2bd5af2c31a5124bcab400da
AhnYeonghoo <dksdudgn00@naver.com>
2022-10-25 18:58:47 +0900

    d

---
+++ Python/OpenSourceDevPrac/ch3/3.py
@@ -0,0 +1,7 @@
+print('Hello, World!')
+print('Python Programming')
+
+# 3.8 심사문제
+
+print('Hello, world!')
+print('Hello, world!')
---
+++ Python/OpenSourceDevPrac/ch4/4.py
@@ -0,0 +1,7 @@
+print('Hello, world!')
+print('Hello'); print('1234')
+
+# 4.2 comment
+
+# Hello, world! 출력
+print('Hello world!')
