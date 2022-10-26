dcb34798462b5007df16bdff7349a1bf60112d9b
AhnYeonghoo <dksdudgn00@naver.com>
2022-10-26 16:59:24 +0900

    d

--- Python/OpenSourceDevPrac/ch4/4.py
+++ Python/OpenSourceDevPrac/ch4/4.py
@@ -5,3 +5,11 @@ print('Hello'); print('1234')
 
 # Hello, world! 출력
 print('Hello world!')
+
+a = [[10, 20], [30, 40], [50, 60]]
+
+for i in a:
+    for j in i:
+        print(j, end=' ')
+    print()
+
