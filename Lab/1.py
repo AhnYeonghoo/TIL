14bf172e46a1be07caeacfa1d61b454ac6ba0953
AhnYeonghoo <dksdudgn00@naver.com>
2022-11-01 13:33:53 +0900

    d

--- Lab/1.py
+++ Lab/1.py
@@ -3,19 +3,19 @@ import numpy as np
 import pandas as pd
 import sklearn.linear_model
 
-oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
-gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values='n/a')
+# oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+# gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values='n/a')
 
-country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+# country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
 X = np.c_[country_stats["GDP per capita"]]
 y = np.c_[country_stats["Life satisfaction"]]
 
-country_stats.plot(kind="scatter", x="GDP per capita", y="Life satisfaction")
-plt.show()
+# # country_stats.plot(kind="scatter", x="GDP per capita", y="Life satisfaction")
+# plt.show()
 
 model = sklearn.linear_model.LinearRegression()
 
-model.fix(X, y)
-
+model.fit(X, y)
+print(model)
 X_new = [[22587]]
 print(model.predict(X_new))
