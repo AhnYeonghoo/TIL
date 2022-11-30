3abedbef37ee409eac7a1243cd0ee8e12a040502
AhnYeonghoo <dksdudgn00@naver.com>
2022-11-30 15:23:23 +0900

    k

--- C_C++/CppStory/02-02.cpp
+++ C_C++/CppStory/02-02.cpp
@@ -15,5 +15,42 @@ int main(void)
     std::cout << sizeof(num1) << std::endl;
     std::cout << sizeof(num2) << std::endl;
     std::cout << sizeof(num3) << std::endl;
+    std::cout << "\n\n";
+    int arr[3] =  {1, 2, 3};
+    
+    int& ref1 = arr[0];
+    int& ref2 = arr[1];
+    int& ref3 = arr[2];
+    
+    int* ptr1 = &arr[0];
+    int* ptr2 = &arr[1];
+    int* ptr3 = &arr[3];
+    
+    std::cout << ref1 << std::endl;
+    std::cout << ref2 << std::endl;
+    std::cout << ref3 << std::endl;
+
+    std::cout << ptr1 << std::endl;
+    std::cout << ptr2 << std::endl;
+    std::cout << ptr3 << std::endl;
+
+    std::cout << *ptr1 << std::endl;
+    std::cout << *ptr2 << std::endl;
+    std::cout << *ptr3 << std::endl;
+
+    int num = 12;
+    int* ptr = &num;
+    int** dptr = &ptr;
+    
+    int& ref = num;
+    int*& pref = ptr;
+    int**& dpref = dptr;
+    
+    std::cout << ref << std::endl;
+    std::cout << *pref << std::endl;
+    std::cout << **dptr << std::endl;
+
+
+
     return 0;
 }
---
+++ C_C++/CppStory/02-03.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+void SwapByRef2(int& ref1, int& ref2) 
+{
+    int temp = ref1;
+}
