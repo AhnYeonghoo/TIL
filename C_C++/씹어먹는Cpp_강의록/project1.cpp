819447bf23497399a0e9c2a3c34b17c3940e47d0
AhnYeonghoo <dksdudgn00@naver.com>
2022-12-06 17:54:57 +0900

    Project1 comple

--- C_C++/씹어먹는Cpp_강의록/project1.cpp
+++ C_C++/씹어먹는Cpp_강의록/project1.cpp
@@ -106,3 +106,117 @@ MyString& MyString::assign(const char* str) {
 
     return *this;
 }
+
+int MyString::capacity() const { return memory_capacity; }
+
+void MyString::reserve(int size) {
+    if (size > memory_capacity) {
+
+        char* prev_string_content = string_content;
+        string_content = new char[size];
+        memory_capacity = size;
+        
+        for (int i = 0; i != string_length; i++)
+            string_content[i] = prev_string_content[i];
+
+        delete[] prev_string_content;
+    }
+}
+
+char MyString::at(int i) const {
+    if (i >= string_length || i < 0)
+        return 0;
+    else  
+        return string_content[i];
+}
+
+MyString& MyString::insert(int loc, const MyString& str) {
+    if (loc < 0  || loc > string_length) return *this;
+    if (string_length + str.string_length > memory_capacity) {
+        if (memory_capacity * 2 > string_length + str.string_length)
+            memory_capacity *= 2;
+        else 
+            memory_capacity = string_length + str.string_length;
+        
+        char* prev_string_content = string_content;
+        string_content = new char[memory_capacity];
+        
+        int i;
+        for (i = 0; i < loc; i++) {
+            string_content[i] = prev_string_content[i];
+        }
+        
+        for (int j = 0; j != str.string_length; j++) {
+            string_content[i+j] = str.string_content[j];
+        }
+        
+        for (; i < string_length; i++) {
+            string_content[str.string_length + i] = prev_string_content[i];
+        }
+        
+        delete[] prev_string_content;
+
+        string_length = string_length + str.string_length;
+        return *this;
+    }
+
+    for (int i = string_length - 1; i >= loc; i--) {
+        string_content[i + str.string_length] = string_content[i];
+    }
+    
+    for (int i = 0; i < str.string_length; i++) 
+        string_content[i + loc] = str.string_content[i];
+
+    string_length = string_length + str.string_length;
+    return *this;
+}
+
+MyString& MyString::insert(int loc, const char* str) {
+    MyString temp(str);
+    return insert(loc, temp);
+}
+
+MyString& MyString::insert(int loc, char c) {
+    MyString temp(c);
+    return insert(loc, temp);
+}
+
+MyString& MyString::erase(int loc, int num) {
+    if (num < 0 || loc < 0 || loc > string_length)
+        return *this;
+
+    for (int i = loc + num; i < string_length; i++) {
+        string_content[i - num] = string_content[i];
+    }
+    string_length -= num;
+    return *this;
+}
+
+int MyString::find(int find_from, const MyString& str) const {
+    int i, j;
+    if (str.string_length == 0) return -1;
+    for (i = find_from; i <= string_length - str.string_length; i++) {
+        for (j = 0; j < str.string_length; j++) {
+            if (string_content[i + j] != str.string_content[j]) break;
+        }
+
+        if (j == str.string_length) return i;
+    }
+    return -1;
+}
+
+int MyString::find(int find_from, const char* str) const {
+    MyString temp(str);
+    return find(find_from, temp);
+}
+
+int MyString::find(int find_from, char c) const {
+    MyString temp(c);
+    return find(find_from, temp);
+}
+
+int main() {
+    MyString str1("this is a very very long string");
+    std::cout <<  str1.find(0, "very") << std::endl;
+    std::cout << str1.find(str1.find(0, "very") + 1, "very") << std::endl;
+}
