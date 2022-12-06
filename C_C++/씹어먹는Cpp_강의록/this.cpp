21fe10b8531a3a47de4cfd836df815c247739f5a
AhnYeonghoo <dksdudgn00@naver.com>
2022-12-06 11:22:36 +0900

    상수 맴버함수

--- C_C++/씹어먹는Cpp_강의록/this.cpp
+++ C_C++/씹어먹는Cpp_강의록/this.cpp
@@ -1,71 +1,86 @@
 #include <iostream>
 class Marine {
-static int total_marine_num;
-const static int i = 0;
-int hp; // 마린 체력
-int coord_x, coord_y; // 마린 위치
-bool is_dead;
-const int default_damage; // 기본 공격력
-public:
-Marine(); // 기본 생성자
-Marine(int x, int y); // x, y 좌표에 마린 생성
-Marine(int x, int y, int default_damage);
-int attack(); // 데미지를 리턴한다.
-Marine& be_attacked(int damage_earn); // 입는 데미지
-void move(int x, int y); // 새로운 위치객체지향프로그래밍의 시작 126
-void show_status(); // 상태를 보여준다.
-static void show_total_marine();
-~Marine() { total_marine_num--; }
+    static int total_marine_num;
+    const static int i = 0;
+
+    int hp; // 마린 체력
+    int coord_x, coord_y; // 마린 위치
+    bool is_dead;
+
+    const int default_damage; 
+    // 기본 공격력
+    public:
+        Marine(); // 기본 생성자
+        Marine(int x, int y); // x, y 좌표에 마린 생성
+        Marine(int x, int y, int default_damage);
+
+        int attack(); // 데미지를 리턴한다.
+        Marine& be_attacked(int damage_earn); // 입는 데미지
+        void move(int x, int y); // 새로운 위치객체지향프로그래밍의 시작 126
+        void show_status(); // 상태를 보여준다.
+
+        static void show_total_marine();
+        ~Marine() { total_marine_num--; }
 };
+
 int Marine::total_marine_num = 0;
+
 void Marine::show_total_marine() {
-std::cout << "전체 마린 수 : " << total_marine_num << std::endl;
+    std::cout << "전체 마린 수 : " << total_marine_num << std::endl;
 }
+
 Marine::Marine()
 : hp(50), coord_x(0), coord_y(0), default_damage(5), is_dead(false) {
 total_marine_num++;
 }
+
 Marine::Marine(int x, int y)
-: coord_x(x),
-coord_y(y),
-hp(50),
-default_damage(5),
-is_dead(false) {
-total_marine_num++;
+    : coord_x(x),
+      coord_y(y),
+      hp(50),
+      default_damage(5),
+      is_dead(false) {
+    total_marine_num++;
 }
+
 Marine::Marine(int x, int y, int default_damage)
-: coord_x(x),
-coord_y(y),
-hp(50),
-default_damage(default_damage),
-is_dead(false) {
-total_marine_num++;
+    : coord_x(x),
+      coord_y(y),
+      hp(50),
+      default_damage(default_damage),
+      is_dead(false) {
+    total_marine_num++;
 }
+
 void Marine::move(int x, int y) {
-coord_x = x;
-coord_y = y;
+    coord_x = x;
+    coord_y = y;
 }
+
 int Marine::attack() { return default_damage; }
+
 Marine& Marine::be_attacked(int damage_earn) {
-hp -= damage_earn;
-if (hp <= 0) is_dead = true;
-return *this;
+    hp -= damage_earn;
+    if (hp <= 0) is_dead = true;
+    return *this;
 }
+
 void Marine::show_status() {
-std::cout << " *** Marine *** " << std::endl;
-std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
-<< std::endl;
-std::cout << " HP : " << hp << std::endl;
-std::cout << " 현재 총 마린 수 : " << total_marine_num << std::endl;
+    std::cout << " *** Marine *** " << std::endl;
+    std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
+    << std::endl;
+    std::cout << " HP : " << hp << std::endl;
+    std::cout << " 현재 총 마린 수 : " << total_marine_num << std::endl;
 }
 
 int main() {
-Marine marine1(2, 3, 5);
-marine1.show_status();
-Marine marine2(3, 5, 10);
-marine2.show_status();
-std::cout << std::endl << "마린 1 이 마린 2 를 두 번 공격! " << std::endl;
-marine2.be_attacked(marine1.attack()).be_attacked(marine1.attack());
-marine1.show_status();
-marine2.show_status();
+
+    Marine marine1(2, 3, 5);
+    marine1.show_status();
+    Marine marine2(3, 5, 10);
+    marine2.show_status();
+    std::cout << std::endl << "마린 1 이 마린 2 를 두 번 공격! " << std::endl;
+    marine2.be_attacked(marine1.attack()).be_attacked(marine1.attack());
+    marine1.show_status();
+    marine2.show_status();
 }
