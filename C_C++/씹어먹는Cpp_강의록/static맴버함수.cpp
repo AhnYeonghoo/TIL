5d818044ffbd1bb16af9cb7add0ef4a95c45e200
AhnYeonghoo <dksdudgn00@naver.com>
2022-12-06 11:08:36 +0900

    static 맴버함수 및 this 키워드

---
+++ C_C++/씹어먹는Cpp_강의록/static맴버함수.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+class Marine {
+    static int total_marine_num;
+    const static int i = 0;
+    
+    int hp;
+    int coord_x, coord_y;
+    bool is_dead;
+    const int default_damage;
+    
+    public:
+        Marine();
+        Marine(int x, int y);
+        Marine(int x, int y, int default_damage);
+        
+        int attack();
+        void BeAttacked(int damage_earn);
+        void move(int x, int y);
+        void ShowStatus();
+        static void ShowTotalMarine();
+
+        ~Marine() { total_marine_num--; }
+};
+
+int Marine::total_marine_num = 0;
+
+void Marine::ShowTotalMarine() {
+    std::cout << "total marine number: " << total_marine_num << std::endl;
+}
+
+Marine::Marine() : hp(50), coord_x(0), coord_y(0), default_damage(5), is_dead(false) {
+    total_marine_num++;
+}
+
+Marine::Marine(int x, int y)
+    : coord_x(x), coord_y(y), hp(50), default_damage(5), is_dead(false) {
+        total_marine_num++;
+}
+
+Marine::Marine(int x, int y, int default_damage)
+    : coord_x(x),
+      coord_y(y),
+      hp(50),
+      default_damage(default_damage),
+      is_dead(false) {
+        total_marine_num++;
+}
+
+void Marine::move(int x, int y) {
+    coord_x = x;
+    coord_y = y;
+}
+
+int Marine::attack() { return default_damage; }
+
+void Marine::BeAttacked(int damage_earn) {
+    hp -= damage_earn;
+    if (hp <= 0) is_dead = true;
+}
+
+void Marine::ShowStatus() {
+    std::cout << " show status " << std::endl;
+} 
+
+void CreateMarine() {
+    Marine marine3(10, 10, 4);
+    Marine::ShowTotalMarine();
+}
+
+int main() {
+    Marine marine1(2, 3, 5);
+    Marine::ShowTotalMarine();
+
+    Marine marine2(3, 5, 10);
+    Marine::ShowTotalMarine();
+
+    CreateMarine();
+    
+}
