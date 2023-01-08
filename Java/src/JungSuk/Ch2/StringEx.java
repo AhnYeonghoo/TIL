55b3beaf6ab758fc2ce1dae57f5ae1e189182259
AhnYeonghoo <dksdudgn00@naver.com>
2023-01-08 14:25:55 +0900

    연산자까지

---
+++ Java/src/JungSuk/Ch2/StringEx.java
@@ -0,0 +1,11 @@
+package Ch2;
+
+public class StringEx {
+    public static void main(String[] args) {
+        String name = "Ja" + "va";
+        String str = name + 8.0;
+        System.out.println(name);
+        System.out.println(str);
+
+    }
+}
---
+++ Java/src/JungSuk/Ch3/OperatorEx1.java
@@ -0,0 +1,20 @@
+package Ch3;
+
+public class OperatorEx1 {
+    public static void main(String[] args) {
+        int i =5;
+        i++;
+        System.out.println(i);
+        i = 5;
+        ++i;
+        System.out.println(i);
+        int j = 5, k = 5;
+        System.out.println(j++);
+        System.out.println(++k);
+
+//        char c = 'a';
+//        for (int i = 0; i < 26; i++) {
+//            System.out.print(c++);
+//        }
+    }
+}
