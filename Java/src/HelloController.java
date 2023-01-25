ff2d205f86f20905f253113135e19886d50da8bb
AhnYeonghoo <dksdudgn00@naver.com>
2023-01-25 23:06:01 +0900

    스프링

---
+++ Java/src/HelloController.java
@@ -0,0 +1,79 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public static String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name="name", required=false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @GetMapping("hello-website")
+    @ResponseBody
+    public Human helloHuman(@RequestParam("name") String name, @RequestParam("age") int age) {
+        Human human = new Human();
+        human.setName(name);
+        human.setAge(age);
+        return human;
+    }
+
+    static class Human {
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
+    static class Hello {
+        private String name;
+
+
+        public String getName() {
+            return name;
+
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
